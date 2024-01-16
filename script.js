4817cede8041bd5a71a56444b69a83039ab76336
inclined4X <jezemiahsam48@gmail.com>
2024-01-16 11:36:40 +0000

    Quiz App built with HTML, CSS and JavaScript

---
+++ script.js
@@ -0,0 +1,40 @@
+const questions = [
+
+    //first question
+    {
+        question: "which is largest animal in the world?",
+        answers: [
+            { text: "shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    },
+    
+
+    //second question
+    {
+        question: "which is largest animal in the world?",
+        answers: [
+            { text: "shark", correct: false},
+            { text: "Blue Whale", correct: true},
+            { text: "Elephant", correct: false},
+            { text: "Giraffe", correct: false},
+        ]
+    }
+
+
+
+    // third question
+    {
+        question: "which is largest desert in the world?",
+        answers: [
+            { text: "Kalahari", correct: false},
+            { text: "Gobi", correct: true},
+            { text: "Sahara", correct: false},
+            { text: "Antarctica", correct: true},
+        ]
+    }
+
+    
+]
