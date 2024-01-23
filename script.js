497efaebc688bea2d3c3f820d038badd5d4265ba
inclined4X <jezemiahsam48@gmail.com>
2024-01-23 16:57:53 +0000

    added functions for when the the next button is clicked on what happens and also the score at the end of the questions

--- script.js
+++ script.js
@@ -99,6 +99,7 @@ function selectAnswer(e){
     const isCorrect = selectedBtn.dataset.correct === "true";
     if(isCorrect){
         selectedBtn.classList.add('correct');
+        score++
     }else{
         selectedBtn.classList.add('incorrect');
     }
@@ -112,6 +113,33 @@ function selectAnswer(e){
     });
     nextButton.style.display = 'block';
 }
+
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = ` You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else{
+        showScore();
+    }
+}
+
+nextButton.addEventListener('click', () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    }else{
+        StartQuiz()
+    }
+})
 
 
 StartQuiz(); 
